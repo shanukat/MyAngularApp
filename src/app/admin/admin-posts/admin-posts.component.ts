57b586df0b17ea70efbc29659c9aa7563273be73
shanukat <shanukato@gmail.com>
2025-04-12 12:57:31 +0800

    add all files

---
+++ src/app/admin/admin-posts/admin-posts.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-posts',
+  imports: [],
+  templateUrl: './admin-posts.component.html',
+  styleUrl: './admin-posts.component.css'
+})
+export class AdminPostsComponent {
+
+}
